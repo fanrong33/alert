855afa07dde66367e2dfad3e765b0ae7ba310a66
fanrong33 <fanrong33@qq.com>
2017-10-18 21:18:29 +0800

    新增通知策略页面

---
+++ code/Panel/Lib/Action/NotificationAction.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 通知策略 控制器类
+ * @author 蔡繁荣
+ * @version  1.0.0 build 20171018
+ */
+class NotificationAction extends CommonAction{
+
+    public function index(){
+
+        $this->display();
+    }
+
+}
